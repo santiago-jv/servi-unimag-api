5ad32bb7c6d2c428ebf3c6256c387a3a3ede98e2
Santiago <santiago.olayo@bolsiyo.app>
2023-05-18 22:48:22 -0500

    fix: repair damages

--- src/subject/subject.module.ts
+++ src/subject/subject.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SubjectService } from './subject.service';
 import { SubjectController } from './subject.controller';
+import { Subject } from './subject.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   providers: [SubjectService],
-  controllers: [SubjectController]
+  controllers: [SubjectController],
+  imports:[TypeOrmModule.forFeature([Subject]), UserModule],
 })
 export class SubjectModule {}
